a1a442cc1cf49b056274aea821c27289757d40ef
Lichter <Christopher.Lichter@se2.com>
2016-09-30 17:13:35 -0500

    Started work on models.

---
+++ models.py
@@ -0,0 +1,4 @@
+"""models.py - This file contains the class definitions for the Datastore
+entities used by the Hangman game. Because these classes are also regular Python
+classes they can include methods (such as 'to_form' and 'new_game')."""
+
