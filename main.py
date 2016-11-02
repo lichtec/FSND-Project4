ab6c56681246f9869788e176bb4245ab5c839cc3
Lichter <Christopher.Lichter@se2.com>
2016-11-02 17:40:19 -0500

    main.py pep8

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 ##!/usr/bin/env python
 #
-#"""main.py - This file includes generic landing page handler and handlers that are called by taskqueue and/or cronjobs."""
+"""main.py - This file includes generic landing page
+   handler and handlers that are called by taskqueue and/or cronjobs."""
 
 import logging
 import webapp2
@@ -15,9 +16,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def welcome():
     return 'App is serving'
+
 
 @app.errorhandler(500)
 def server_error(e):
@@ -25,17 +28,20 @@ def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 
+
 class SendConfirmationEmailHandler(webapp2.RequestHandler):
     def post(self):
         """Send email regarding challenge."""
 
         mail.send_mail(
             'noreply@%s.appspotmail.com' % (
-                app_identity.get_application_id()),     # from
-            self.request.get('email'),                  # to
-            'You have been challenged by {}'.format(self.request.get('challenger')),            # subj
+                app_identity.get_application_id()),
+            self.request.get('email'),
+            'You have been challenged by {}'.format(
+                self.request.get('challenger')),
             '{}'.format(self.request.get('gameInfo'))
         )
+
 
 class SendEndGameEmailHandler(webapp2.RequestHandler):
     def post(self):
@@ -43,23 +49,28 @@ class SendEndGameEmailHandler(webapp2.RequestHandler):
 
         mail.send_mail(
             'noreply@%s.appspotmail.com' % (
-                app_identity.get_application_id()),# from
-            self.request.get('email'),# to
-            '{} Completed Your Challenge'.format(self.request.get('challenged')),            # subj
+                app_identity.get_application_id()),
+            self.request.get('email'),
+            '{} Completed Your Challenge'.format(
+                self.request.get('challenged')),
             '{}'.format(self.request.get('gameInfo'))
         )
+
 
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
         """Send a reminder email to each User that has an active game
         runs every 12 hours"""
         app_id = app_identity.get_application_id()
-        users = User.query(User.email != None)
+        users = User.query(User.email is not None)
         reminder = []
         for user in users:
-            if(Game.query(ndb.AND(Game.game_over == False,
-                                  ndb.AND(Game.cancel == False,
-                                          Game.challenged == user.key))).count() >0):
+            if(Game.query(
+                ndb.AND(
+                    Game.game_over == False,
+                    ndb.AND(
+                        Game.cancel == False,
+                        Game.challenged == user.key))).count() > 0):
                 reminder.append(user)
         for user in reminder:
             subject = 'This is a reminder!'
@@ -70,6 +81,7 @@ class SendReminderEmail(webapp2.RequestHandler):
                            user.email,
                            subject,
                            body)
+
 
 app = webapp2.WSGIApplication([
     ('/crons/send_reminder', SendReminderEmail),
